7bae9534083bdbfa8e8f0c7225857cfbc4cba23d
Artem <theuselessn@gmail.com>
2020-06-29 21:37:07 +0600

    services js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Main Slider
 var slideRadios = document.querySelectorAll(".slider-radio");
 var sliderContent = document.querySelectorAll(".card");
 
@@ -20,3 +21,25 @@ function sliderFunction() {
     sliderContent[1].classList.remove("card-active");
   }
 }
+// Services
+var serviceButtons = document.querySelectorAll(".service-name");
+
+serviceButtons.forEach(function (item) {
+  item.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    var currentSlide = item.dataset.serviceCotrol;
+
+    document
+      .querySelector(".service-name" + ".service-name-active")
+      .classList.remove("service-name-active");
+    item.classList.add("service-name-active");
+
+    document
+      .querySelector(".services-descr-item" + ".descr-item-active")
+      .classList.remove("descr-item-active");
+    document
+      .querySelector('[data-service-descr="' + currentSlide + '"]')
+      .classList.add("descr-item-active");
+  });
+});
