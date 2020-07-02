0c0c1f36223c6a2533de1167814fbaae9f2b3bbb
Artem <theuselessn@gmail.com>
2020-07-02 21:49:47 +0600

    pixel perfect v2, modals, all js

--- js/script.js
+++ js/script.js
@@ -40,3 +40,63 @@ serviceButtons.forEach(function (item) {
       .classList.add("descr-item-active");
   });
 });
+// Write Us Modal
+var writeUsTrigger = document.querySelector(".write-us-trigger");
+var writeUsForm = document.querySelector(".write-us");
+var modalClose = document.querySelector(".modal-close");
+
+var writeUsName = document.querySelector(".write-input-name");
+var writeUsEmail = document.querySelector(".write-input-email");
+
+writeUsTrigger.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  writeUsForm.classList.add("modal-show");
+  writeUsName.focus();
+});
+
+modalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  writeUsForm.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (writeUsForm.classList.contains("modal-show")) {
+      writeUsForm.classList.remove("modal-show");
+      writeUsForm.classList.remove("modal-error");
+    }
+  }
+});
+// Write Us Error
+writeUsForm.addEventListener("submit", function (evt) {
+  if (!writeUsName.value || !writeUsEmail.value) {
+    evt.preventDefault();
+    writeUsForm.classList.remove("modal-error");
+    writeUsForm.offsetWidth = writeUsForm.offsetWidth;
+    writeUsForm.classList.add("modal-error");
+  }
+});
+// Map Modal
+var mapTrigger = document.querySelector(".device-map");
+var mapModal = document.querySelector(".our-location");
+var mapModalClose = document.querySelector(".map-modal-close");
+
+mapTrigger.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapModal.classList.add("modal-show");
+});
+
+mapModalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapModal.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (mapModal.classList.contains("modal-show")) {
+      mapModal.classList.remove("modal-show");
+    }
+  }
+});
